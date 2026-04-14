1f059561d0205e90f7beeee93b20cf8950f8c9d4
wtfx0rzz-mark <wtfmark@pm.me>
2026-04-14 18:45:01 -0500

    Create ui.lua

---
+++ ui.lua
@@ -0,0 +1,57 @@
+-- ui.lua
+local Players = game:GetService("Players")
+local lp = Players.LocalPlayer
+
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "Zombie Testing",
+    Icon = "crosshair",
+    Author = "Mark",
+    Folder = "zombietest",
+    Size = UDim2.fromOffset(520, 360),
+    Transparent = false,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 150,
+    HideSearchBar = false,
+    ScrollBarEnabled = true,
+    User = {
+        Enabled = true,
+        Anonymous = false,
+        Callback = function()
+            WindUI:Notify({
+                Title = "User Info",
+                Content = "Logged In As: " .. (lp.DisplayName or lp.Name),
+                Duration = 2,
+                Icon = "user",
+            })
+        end,
+    },
+})
+
+Window:SetToggleKey(Enum.KeyCode.V)
+
+local Tabs = {
+    Combat = Window:Tab({
+        Title = "Combat",
+        Icon = "swords",
+        Desc = "Auto hit / targeting",
+    }),
+    Items = Window:Tab({
+        Title = "Items",
+        Icon = "package",
+        Desc = "Item magnet / pickup",
+    }),
+    Player = Window:Tab({
+        Title = "Player",
+        Icon = "user",
+        Desc = "Movement / jump / interactions",
+    }),
+}
+
+return {
+    Lib = WindUI,
+    Window = Window,
+    Tabs = Tabs,
+}
